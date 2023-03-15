314401c12f9fd42f6b2ecf7bb509eca0f3de7bf3
ChiforAlexandru <alexandru.chifor@student.upt.ro>
2023-03-15 19:07:06 +0200

    Program upload

---
+++ src/main/java/com/books/book/api/controller/BookController.java
@@ -0,0 +1,56 @@
+package com.books.book.api.controller;
+
+import com.books.book.api.model.Book;
+import com.books.book.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService){
+        this.bookService=bookService;
+    }
+
+    @GetMapping("/library/search")
+    public Book getBook(@RequestParam String title){
+        Optional<Book> book=bookService.getBook(title);
+        if (book.isPresent()){
+            return (Book) book.get();
+        }
+        else return null;
+    }
+
+    @GetMapping("/library/add")
+    public String addBook(@RequestParam String title, String author){
+        bookService.addBook(title, author);
+        return "Book added: Title:"+title+" Author:"+author;
+    }
+
+    @GetMapping("/library/books")
+    public List<Book> listAllBooks(){
+        return bookService.listAllBooks();
+    }
+
+    @GetMapping("/library/update")
+    public String updateAuthor(@RequestParam String title, String newAuthor){
+        bookService.updateAuthor(title,newAuthor);
+        return "Book author updated: Title:"+title+" Author:"+newAuthor;
+    }
+
+    @GetMapping("/library/delete")
+    public String deleteBook(@RequestParam String title){
+        if (!bookService.deleteBook(title))
+            return "Book deleted: Title:"+title;
+        else return "Book does not exist";
+    }
+}
---
+++ src/main/java/com/books/book/service/BookService.java
@@ -0,0 +1,66 @@
+package com.books.book.service;
+
+import com.books.book.api.model.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class BookService {
+
+    private List<Book> bookList;
+
+    public BookService(List<Book> booksList) {
+        this.bookList = new ArrayList<>();
+
+        Book book1=new Book("titlu1", "autor1");
+        Book book2=new Book("titlu2", "autor2");
+        Book book3=new Book("titlu3", "autor3");
+        Book book4=new Book("titlu4", "autor4");
+        Book book5=new Book("titlu5", "autor5");
+
+        bookList.addAll(Arrays.asList(book1,book2,book3,book4,book5));
+    }
+
+    public Optional<Book> getBook(String title) {
+        Optional optional=Optional.empty();
+        for (Book book:bookList){
+            if (Objects.equals(title, book.getTitle())){
+                optional=Optional.of(book);
+                return optional;
+            }
+        }
+        return optional;
+    }
+
+    public void addBook(String title, String author){
+        Book book=new Book(title, author);
+        bookList.add(book);
+    }
+
+    public void updateAuthor(String title, String newAuthor){
+        Book book1=new Book("","");
+        for (Book book:bookList){
+            if (Objects.equals(title, book.getTitle())){
+                book1=book;
+            }
+        }
+        bookList.remove(book1);
+        book1.setAuthor(newAuthor);
+        bookList.add(book1);
+    }
+
+    public boolean deleteBook(String title){
+        Book book1=new Book(title, "");
+        for (Book book:bookList){
+            if (Objects.equals(book1.getTitle(), book.getTitle())){
+                return bookList.remove(book);
+            }
+        }
+        return false;
+    }
+
+    public List<Book> listAllBooks(){
+        return bookList;
+    }
+}
